e897dcfe9c194355e56dd00712f3ce300d642fae
Kamila Nasibullina <95752969+notcami@users.noreply.github.com>
2024-11-27 02:30:38 +0300

    Update circle.py

--- circle.py
+++ circle.py
@@ -1,10 +1,19 @@
 import math
-
+'''Импорт библиотеки math для работы с числом pi'''
 
 def area(r):
-    return math.pi * r * r
+    '''Принимает радиус окружности, возвращает площадь окружности'''
+    if str(r).isalpha() == 1:
+        return TypeError
+    if r <= 0:
+        return ValueError
+    return (math.pi * r * r)
+    
 
-
-def perimeter(r):
+def perimetr(r):
+    '''Функция получает радиус, вычисляет периметр окружности'''
+    if str(r).isalpha() == 1:
+        return TypeError
+    if r <= 0:
+        return ValueError
     return 2 * math.pi * r
-
