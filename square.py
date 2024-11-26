b30e269385bfba812cc8ed5cb6b9f4bd09dd5e31
Kamila Nasibullina <95752969+notcami@users.noreply.github.com>
2024-11-27 02:31:06 +0300

    Update square.py

--- square.py
+++ square.py
@@ -1,7 +1,17 @@
-
 def area(a):
+    if str(a).isalpha() == 1:
+        return TypeError
+    if a <= 0:
+        return ValueError
     return a * a
 
+    '''Программа получает число a , возвращает a * a (площадь квадрата)''' 
 
-def perimeter(a):
+def perimetr(a):
+    if str(a).isalpha() == 1:
+        return TypeError
+    if a <= 0:
+        return ValueError
     return 4 * a
+
+    '''Программа получает число a , возвращает a*4 (периметр квадрата)'''
