c3d818aada8d27c8130a2c7f2dadd3e4e7dbf835
Kamila Nasibullina <95752969+notcami@users.noreply.github.com>
2024-11-27 02:59:54 +0300

    Update circle.py

--- circle.py
+++ circle.py
@@ -3,17 +3,17 @@ import math
 
 def area(r):
     '''Принимает радиус окружности, возвращает площадь окружности'''
-    if str(r).isalpha() == 1:
-        return TypeError
+    if not isinstance(r, (int, float)):
+        raise TypeError("Радиус должен быть числом")
     if r <= 0:
-        return ValueError
-    return (math.pi * r * r)
-    
+        raise ValueError("Радиус должен быть положительным")
+    return math.pi * r * r
 
 def perimetr(r):
     '''Функция получает радиус, вычисляет периметр окружности'''
-    if str(r).isalpha() == 1:
-        return TypeError
+    if not isinstance(r, (int, float)):
+        raise TypeError("Радиус должен быть числом")
     if r <= 0:
-        return ValueError
+        raise ValueError("Радиус должен быть положительным")
     return 2 * math.pi * r
+
